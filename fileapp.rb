7ac82d272b9c3724347c4c1345588c78e650cbcc
kikikrestanto <kikikrestanto@gmail.com>
2018-01-06 15:32:14 +0700

    mencoba commit

---
+++ fileapp.rb
@@ -0,0 +1,20 @@
+
+require "sinatra"
+get "/" do
+	"done"
+	end
+get "/tada" do
+	"tada"
+end
+
+get "/mali" do
+	"mali"
+end
+
+get "/tangguh" do
+	"tangguh"
+end
+
+get "/wadaw" do
+	"Nama nya juga hidup, hidup juga namanya"
+end
