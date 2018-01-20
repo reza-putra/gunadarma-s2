54405a6da4d9fe8be9453c9dc2768d78b75eeb8e
kikikrestanto <kikikrestanto@gmail.com>
2018-01-20 14:10:28 +0700

    update component from

--- frontend/app/src/Components/Form/MyForm.js
+++ frontend/app/src/Components/Form/MyForm.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {Component} from "react";
 import MyInput from "./MyInput";
 
-const MyForm = props => {
-  return (
-    <form>
-      <MyInput name="test" type="email" />
-    </form>
-  );
-};
+class MyForm extends Component {
+  renderData(data){
+    return (
+    	<MyInput name={data.name} type={data.type} label={data.label} />
+    );
+    	
+  }
 
-export default MyForm;
+  render(){
+  	let data = [
+  		{ name:"test", type:"email", label: "email" },
+        { name:"name", type:"text", label: "name"}
+        { name:"password", type: "password",
+        	label: "password" }
+
+        ];
+return <from>{data.map(this.renderData)}</from?;
+	}
+}
